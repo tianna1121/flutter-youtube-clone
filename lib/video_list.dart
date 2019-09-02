d89e39f9b627ea8623701d01578126f008f0bd3e
Fred Jonason <fredjonason@gmail.com>
2019-09-02 17:36:25 +0800

    implement the landscape model and use MediaQuery to swap between two models

--- lib/video_list.dart
+++ lib/video_list.dart
@@ -7,33 +7,94 @@ class VideoList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final deviceOrientation = MediaQuery.of(context).orientation;
+
     return ListView.separated(
       itemCount: listData.length,
       itemBuilder: (context, index) {
-        return Column(
-          children: <Widget>[
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: 200.0,
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: NetworkImage(listData[index].thumbNail),
-                      fit: BoxFit.cover)),
-            ),
-            ListTile(
-              leading: CircleAvatar(
-                backgroundImage: NetworkImage(listData[index].channelAvatar),
-              ),
-              title: Text(listData[index].title),
-              subtitle: Text(
-                  "${listData[index].channelTitle} ${listData[index].viewCount} ${listData[index].publishedTime}"),
-            )
-          ],
-        );
+        return deviceOrientation != Orientation.portrait
+            ? _buildLandscapeList(context, index)
+            : _buildPortraitList(context, index);
       },
       separatorBuilder: (context, index) => Divider(
         height: 1.0,
         color: Colors.grey,
+      ),
+    );
+  }
+
+  Widget _buildPortraitList(BuildContext context, int index) {
+    return Column(
+      children: <Widget>[
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: 200.0,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: NetworkImage(listData[index].thumbNail),
+                  fit: BoxFit.cover)),
+        ),
+        ListTile(
+          contentPadding: const EdgeInsets.all(8.0),
+          dense: true,
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(listData[index].channelAvatar),
+          ),
+          title: Padding(
+            padding: const EdgeInsets.only(bottom: 4.0),
+            child: Text(listData[index].title),
+          ),
+          subtitle: Text(
+              "${listData[index].channelTitle} ${listData[index].viewCount} ${listData[index].publishedTime}"),
+          trailing: Container(
+              margin: const EdgeInsets.only(bottom: 20.0),
+              child: Icon(Icons.more_vert)),
+        )
+      ],
+    );
+  }
+
+  Widget _buildLandscapeList(BuildContext context, int index) {
+    return Container(
+      padding: const EdgeInsets.all(12.0),
+      child: Row(
+        children: <Widget>[
+          Container(
+            //         width: MediaQuery.of(context).size.width / 2,
+            width: 336.0 / 1.5,
+            height: 188.0 / 1.5,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(listData[index].thumbNail),
+                    fit: BoxFit.cover)),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                ListTile(
+                  contentPadding: const EdgeInsets.all(8.0),
+                  title: Padding(
+                    padding: const EdgeInsets.only(bottom: 4.0),
+                    child: Text(listData[index].title),
+                  ),
+                  subtitle: Text(
+                      "${listData[index].channelTitle} ${listData[index].viewCount} ${listData[index].publishedTime}"),
+                  trailing: Container(
+                      margin: const EdgeInsets.only(bottom: 30.0),
+                      child: Icon(Icons.more_vert)),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: CircleAvatar(
+                    backgroundImage:
+                        NetworkImage(listData[index].channelAvatar),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
