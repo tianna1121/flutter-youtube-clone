a9f0b37bef95af9ac863359a22bc69edfc61b5e0
Fred Jonason <fredjonason@gmail.com>
2019-09-02 16:24:51 +0800

    clone the mainpage of youtube, using bottomNavigationBar()

--- lib/youtube_main.dart
+++ lib/youtube_main.dart
@@ -6,8 +6,64 @@ class YoutubeMain extends StatefulWidget {
 }
 
 class _YoutubeMainState extends State<YoutubeMain> {
+  int _currentIndex = 0;
+  _onTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    List<Widget> _screens = [
+      Center(child: Text("Home")),
+      Center(child: Text("Trending")),
+      Center(child: Text("Subscriptions")),
+      Center(child: Text("Inbox")),
+      Center(child: Text("Library")),
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.grey),
+        title: Image.asset(
+          "assets/images/youtube_logo.png",
+          width: 98.0,
+          height: 22.0,
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.videocam),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.account_circle),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        fixedColor: Colors.red,
+        onTap: _onTapped,
+        items: [
+          BottomNavigationBarItem(title: Text("Home"), icon: Icon(Icons.home)),
+          BottomNavigationBarItem(
+              title: Text("Trending"), icon: Icon(Icons.whatshot)),
+          BottomNavigationBarItem(
+              title: Text("Subsciptions"), icon: Icon(Icons.subscriptions)),
+          BottomNavigationBarItem(title: Text("Inbox"), icon: Icon(Icons.mail)),
+          BottomNavigationBarItem(
+              title: Text("Library"), icon: Icon(Icons.folder)),
+        ],
+      ),
+    );
   }
 }
